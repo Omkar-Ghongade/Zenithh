178a017c0f0125699f4eeb917c5916561c4b8f32
Achyut21 <achyutkatiyar21@gmail.com>
2024-08-08 01:50:23 +0530

    Profile Navbar and Sign In page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import MarketPlace from './components/MarketPlace';
 import Profile from './components/Profile';
 import Stream from './components/Stream';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
 
 export default function App() {
   const [balance, setBalance] = useState(null);
@@ -24,14 +25,15 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={balance !== null ? <Profile /> : <Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/marketplace" element={<MarketPlace />} />
-        <Route path="/stream" element={<Stream />} />
-        <Route path="/achievements" element={<Achievements />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+        <div className="fixed top-0 -z-10 h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        </div>
+      <div className='container mx-auto px-8'>
+        <Navbar/>
+      </div>
+      
+    </div>
+ 
   );
 }
